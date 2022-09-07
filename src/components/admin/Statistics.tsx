5a73419bbec2291c55db3a37154483766b6b0d80
Agam More <agam@masterschool.com>
2022-09-07 12:37:06 +0300

    refactor: compartmentalize the Statistics ul

---
+++ src/components/admin/Statistics.tsx
@@ -0,0 +1,10 @@
+export const Statistics = () => {
+  return (
+    <ul className='mt-4 text-gray-800'>
+      <li className='mt-2'>Subscribers: NaN</li>
+      <li className='mt-2'>Open rate: NaN</li>
+      <li className='mt-2'>Click rate: NaN</li>
+      <li className='mt-2'>Delivery rate: NaN</li>
+    </ul>
+  );
+};
