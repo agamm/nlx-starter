aeee09fc84900ca640457ef85c1928d6932b1243
Agam More <agam@masterschool.com>
2022-09-08 15:42:26 +0300

    fix: fix global typescript errors

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient;
+}
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
